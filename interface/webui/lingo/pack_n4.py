a73c7a1ba5d044e5658f4c888a22498930c792ae
OppaAI <oppa.ai.org@gmail.com>
2026-09-09 11:44:19 -0700

    Add N4–N1 packs, content_packs, import_bank; wire seed + curated-first Learn

---
+++ interface/webui/lingo/pack_n4.py
@@ -0,0 +1,87 @@
+CSV = """約束|やくそく|promise|kanji|noun
+準備|じゅんび|preparation|kanji|noun
+連絡|れんらく|contact|kanji|noun
+案内|あんない|guidance|kanji|noun
+経験|けいけん|experience|kanji|noun
+計画|けいかく|plan|kanji|noun
+理由|りゆう|reason|kanji|noun
+意味|いみ|meaning|kanji|noun
+意見|いけん|opinion|kanji|noun
+質問|しつもん|question|kanji|noun
+答える|こたえる|to answer|kanji|verb
+急ぐ|いそぐ|to hurry|kanji|verb
+間に合う|まにあう|to be in time|kanji|verb
+始める|はじめる|to begin|kanji|verb
+終わる|おわる|to end|kanji|verb
+続ける|つづける|to continue|kanji|verb
+決める|きめる|to decide|kanji|verb
+思う|おもう|to think|kanji|verb
+考える|かんがえる|to consider|kanji|verb
+教える|おしえる|to teach|kanji|verb
+習う|ならう|to learn|kanji|verb
+覚える|おぼえる|to memorize|kanji|verb
+忘れる|わすれる|to forget|kanji|verb
+借りる|かりる|to borrow|kanji|verb
+貸す|かす|to lend|kanji|verb
+送る|おくる|to send|kanji|verb
+受ける|うける|to receive; take (exam)|kanji|verb
+通う|かよう|to commute|kanji|verb
+便利|べんり|convenient|kanji|na-adj
+不便|ふべん|inconvenient|kanji|na-adj
+簡単|かんたん|simple|kanji|na-adj
+複雑|ふくざつ|complex|kanji|na-adj
+安全|あんぜん|safe|kanji|na-adj
+危険|きけん|dangerous|kanji|na-adj
+必要|ひつよう|necessary|kanji|na-adj
+十分|じゅうぶん|enough|kanji|na-adj
+特別|とくべつ|special|kanji|na-adj
+同じ|おなじ|same|kanji|adj
+違う|ちがう|to differ|kanji|verb
+最近|さいきん|recently|kanji|time
+最初|さいしょ|first|kanji|time
+最後|さいご|last|kanji|time
+将来|しょうらい|future|kanji|noun
+昔|むかし|long ago|kanji|noun
+文化|ぶんか|culture|kanji|noun
+歴史|れきし|history|kanji|noun
+社会|しゃかい|society|kanji|noun
+経済|けいざい|economy|kanji|noun
+政治|せいじ|politics|kanji|noun
+国際|こくさい|international|kanji|noun
+環境|かんきょう|environment|kanji|noun
+自然|しぜん|nature|kanji|noun
+科学|かがく|science|kanji|noun
+技術|ぎじゅつ|technology|kanji|noun
+情報|じょうほう|information|kanji|noun
+新聞|しんぶん|newspaper|kanji|noun
+雑誌|ざっし|magazine|kanji|noun
+番組|ばんぐみ|TV program|kanji|noun
+空港|くうこう|airport|kanji|place
+ホテル|ホテル|hotel|kanji|katakana
+レストラン|レストラン|restaurant|kanji|katakana
+スーパー|スーパー|supermarket|kanji|katakana
+アルバイト|アルバイト|part-time job|kanji|katakana
+テスト|テスト|test|kanji|katakana
+残念|ざんねん|regrettable|kanji|na-adj
+心配|しんぱい|worry|kanji|noun
+安心|あんしん|relief|kanji|noun
+成功|せいこう|success|kanji|noun
+失敗|しっぱい|failure|kanji|noun
+努力|どりょく|effort|kanji|noun
+機会|きかい|opportunity|kanji|noun
+選択|せんたく|choice|kanji|noun
+方法|ほうほう|method|kanji|noun
+結果|けっか|result|kanji|noun
+原因|げんいん|cause|kanji|noun
+目的|もくてき|purpose|kanji|noun
+関係|かんけい|relation|kanji|noun
+比較|ひかく|comparison|kanji|noun
+例えば|たとえば|for example|kanji|phrase
+特に|とくに|especially|kanji|adverb
+必ず|かならず|without fail|kanji|adverb
+ぜひ|ぜひ|by all means|kanji|adverb
+多分|たぶん|probably|kanji|adverb
+そろそろ|そろそろ|soon; it's about time|kanji|adverb
+ちゃんと|ちゃんと|properly|kanji|adverb
+ゆっくり|ゆっくり|slowly|kanji|adverb
+はっきり|はっきり|clearly|kanji|adverb"""
